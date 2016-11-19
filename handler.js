b5feec4ef83c440917299f7de2ec465bc6d4c2c8
takahashim <maki@rubycolor.org>
2016-11-20 01:43:37 +0900

    simplify

--- handler.js
+++ handler.js
@@ -3,7 +3,7 @@
 const Opal = require('./handler.rb.js').Opal;
 
 module.exports.hello = (event, context, callback) => {
-    const env = Opal.JSON.$from_object(event, Opal.hash2([], {}));
+    const env = Opal.JSON.$from_object(event);
     const resOpal = Opal.Hello.$handler(env).$to_n();
     const response = {
         statusCode: resOpal.statusCode,
